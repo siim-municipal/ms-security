b44862ab6f68200e7b7ce4aae302f2c8c9e92a79
Hugo Cobos <hcobos@azurian.com>
2026-01-15 11:33:05 -0600

    feat: Implement CORS configuration in SecurityConfig and update environment API URLs

--- src/main/java/com/tuxoftware/ms_security/config/SecurityConfig.java
+++ src/main/java/com/tuxoftware/ms_security/config/SecurityConfig.java
@@ -29,7 +29,6 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors(Customizer.withDefaults()) // Importante para permitir CORS si el Gateway no lo maneja
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html", "/actuator/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/users/me").authenticated()
